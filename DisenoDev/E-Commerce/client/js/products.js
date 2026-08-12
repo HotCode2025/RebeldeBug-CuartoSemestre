a4938d1cdd9f0be46e6f4d8dd9aa9ce6eb6a0e33
Jimena K Perez <jime3011@gmail.com>
2026-08-11 23:07:52 -0300

    Se completa Clase1 de Disenio Dev

---
+++ DisenoDev/E-Commerce/client/js/products.js
@@ -0,0 +1,46 @@
+const productos =[
+    {
+        id: 1,
+        productName: "Whisky The Glenrothers 12 años Single Malt",
+        price: 258.940,
+        quanty: 1,
+        img:"/client/media/glenrothers.png",
+
+    },
+
+    {
+        id: 2,
+        productName: "Whisky Chivas Regal 18 años",
+        price: 185.080,
+        quanty: 1,
+        img:"/client/media/chivas.png",
+
+    },
+
+    {
+        id: 3,
+        productName: "Vodka Absolute Elix 1L",
+        price: 80.670,
+        quanty: 1,
+        img:"/client/media/absolute.jfif",
+
+    },
+
+    {
+        id: 4,
+        productName: "Licor ST German 700ml",
+        price: 112.800,
+        quanty: 1,
+        img:"/client/media/stgerman.png",
+
+    },
+
+    {
+        id: 5,
+        productName: "Pisco Calavera Reposado 750cc",
+        price: 73.690,
+        quanty: 1,
+        img:"/client/media/pisco.png",
+
+    },
+];
